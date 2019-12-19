2d13e8665bc074be42ca93ccd2eaa514fb03987f
ittsu <ittsu.nkmt@gmail.com>
2019-12-19 17:03:25 +0900

    itemsテーブル作成

--- db/migrate/20191219070555_create_items.rb
+++ db/migrate/20191219070555_create_items.rb
@@ -1,7 +1,18 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-
+      t.string :name, null:false, limit:40
+      t.integer :price, null:false
+      t.text :description, null:false, limit:1000
+      t.references :size, null:false, foreign_key: true
+      t.references :condition, null:false, foreign_key: true
+      t.references :shipping_charge, null:false, foreign_key: true
+      t.references :shipping_method, null:false, foreign_key: true
+      t.references :delivery_area, null:false, foreign_key: true
+      t.references :status, null:false, foreign_key: true
+      t.references :estimated_shipping_date, null:false, foreign_key: true
+      t.references :category, null:false, foreign_key: true
+      t.references :user, null:false, foreign_key: true
       t.timestamps
     end
   end
