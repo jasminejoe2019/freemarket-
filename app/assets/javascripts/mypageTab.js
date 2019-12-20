c963c411da5290c85efed8015c7cb628ea8689d0
ittsu <ittsu.nkmt@gmail.com>
2019-12-21 02:07:47 +0900

    不要なファイルの削除

--- app/assets/javascripts/mypageTab.js
+++ app/assets/javascripts/mypageTab.js
@@ -4,18 +4,18 @@ $(function () {
     $('.active').removeClass("active");
     $(this).addClass("active");
     const index = tabs.index(this);
-    $(".mypage__firstMenu__contents__notice").removeClass("show").eq(index).addClass("show");
+    $(".mypage__firstMenu__contents__list").removeClass("show").eq(index).addClass("show");
   }
   tabs.click(tabSwitch);
 })
 
 $(function () {
   let tabs = $(".mypage__secondMenu__tabs__menu");
-  function tabSwitch2() {
+  function tabSwitch() {
     $('.active2').removeClass("active2");
     $(this).addClass("active2");
     const index = tabs.index(this);
-    $(".mypage__secondMenu__contents__transactionNow").removeClass("show2").eq(index).addClass("show2");
+    $(".mypage__secondMenu__contents__list").removeClass("show2").eq(index).addClass("show2");
   }
-  tabs.click(tabSwitch2);
+  tabs.click(tabSwitch);
 })
