943d423810f5cff0239135b5c7b653843ef09d6e
honda0924 <honda0924@hotmail.co.jp>
2019-12-25 20:10:10 +0900

    delete_capibara

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+dscribe Item do
+  describe "name"
+    it "not_null" do
+    end
 end
