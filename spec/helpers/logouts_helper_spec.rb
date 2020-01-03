600610252bb856c6d101bb7bc9de0ac2c38a9e29
jasminejoe2019 <jasminejoe2019@gmail.com>
2020-01-03 14:59:53 +0900

    logoutコントローラーを修正

--- spec/helpers/logouts_helper_spec.rb
+++ spec/helpers/logouts_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the LogoutHelper. For example:
+# the LogoutsHelper. For example:
 #
-# describe LogoutHelper do
+# describe LogoutsHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-RSpec.describe LogoutHelper, type: :helper do
+RSpec.describe LogoutsHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
