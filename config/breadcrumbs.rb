3164694e4ab3626edbfbd028909506c84307291b
jasminejoe2019 <jasminejoe2019@gmail.com>
2020-01-10 20:49:12 +0900

    パンくず設定ファイルを導入

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,6 +1,31 @@
 crumb :root do
-  link "Home", root_path
+  link "メルカリ", root_path
 end
+
+crumb :mypages do
+  link "マイページ", mypages_path
+  parent :root
+end
+
+crumb :logouts do
+  link "ログアウト", logouts_path
+  parent :mypages
+end
+
+crumb :shipping_addresses do
+  link "本人情報の登録", new_shipping_address_path
+  parent :mypages
+end
+
+crumb :profile do
+  link "プロフィール", edit_mypage_path
+  parent :mypages
+end
+
+# crumb : do
+#   link "商品名", _path
+#   parent :root
+# end
 
 # crumb :projects do
 #   link "Projects", projects_path
