38bc7d63e7c34330f1e2830585bd4bd58249068c
ittsu <ittsu.nkmt@gmail.com>
2020-01-30 17:03:16 +0900

    ビューへDB情報組み込み

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,4 +1,12 @@
 class TradesController < ApplicationController
+  require 'payjp'
+
   def index
+    @item = Item.find(1)
+    
+    payment = Payment.where(user_id: current_user.id).first
+      Payjp.api_key = "sk_test_a02ac9116c8942dbec68459c"
+      customer = Payjp::Customer.retrieve(payment.customer_id)
+      @card_information = customer.cards.retrieve(payment.card_id)
   end
 end
