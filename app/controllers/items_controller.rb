fe7aefa3be4243b782de5f85af88b7b995633949
jasminejoe2019 <jasminejoe2019@gmail.com>
2020-01-30 21:59:58 +0900

    showコントローラー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,7 +6,13 @@ class ItemsController < ApplicationController
     
   end
   def show
-  
+    @item =Item.find(params[:id])
+    @user = User.find(@item.user_id)
+    @conditoon = Condition.find(@item.condition_id)
+    @shipping_charge = ShippingCharge.find(@item.shipping_charge_id)
+    @shipping_method = ShippingMethod.find(@item.shipping_method_id)
+    @delivery_area = DeliveryArea.find(@item.delivery_area_id)
+    @estimated_shipping_date = EstimatedShippingDate.find(@item.estimated_shipping_date_id)
   end
 
 end
