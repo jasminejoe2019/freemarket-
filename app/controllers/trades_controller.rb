e2536cec1d60393db6cdfacafaf7db1b5f57159a
ittsu <ittsu.nkmt@gmail.com>
2020-01-30 18:50:37 +0900

    購入完了ページ作成

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -9,4 +9,13 @@ class TradesController < ApplicationController
       customer = Payjp::Customer.retrieve(payment.customer_id)
       @card_information = customer.cards.retrieve(payment.card_id)
   end
+
+  def new
+    @item = Item.find(1)
+    
+    payment = Payment.where(user_id: current_user.id).first
+      Payjp.api_key = "sk_test_a02ac9116c8942dbec68459c"
+      customer = Payjp::Customer.retrieve(payment.customer_id)
+      @card_information = customer.cards.retrieve(payment.card_id)
+  end
 end
