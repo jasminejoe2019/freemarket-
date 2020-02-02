024e21d434a75e972244390761091244048d92ff
vallee05 <voluptuous.666@gmail.com>
2020-02-02 15:13:21 +0900

    微調整

--- db/migrate/20191219080845_create_categories.rb
+++ db/migrate/20191219080845_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :name, null:false
-      t.integer :tree_parent
+      t.string :ancestry, null:true, multiple_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_01_10_120622) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "tree_parent"
+    t.string "ancestry"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
