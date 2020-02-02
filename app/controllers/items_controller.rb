302d239710b83dbb9ae06ba1da7dbe2daaa1444b
jasminejoe2019 <jasminejoe2019@gmail.com>
2020-02-02 20:40:54 +0900

    condition　のスペルを修正しました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,11 +6,11 @@ class ItemsController < ApplicationController
   def new
 
   end
-  
+
   def show
     @item =Item.find(params[:id])
     @user = User.find(@item.user_id)
-    @conditoon = Condition.find(@item.condition_id)
+    @condition = Condition.find(@item.condition_id)
     @shipping_charge = ShippingCharge.find(@item.shipping_charge_id)
     @shipping_method = ShippingMethod.find(@item.shipping_method_id)
     @delivery_area = DeliveryArea.find(@item.delivery_area_id)
