0d5e112e8baa4bc0f68f5fc87f3c9ffd71150968
jasminejoe2019 <jasminejoe2019@gmail.com>
2020-02-02 20:47:15 +0900

    コントローラーの不要な記述を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,11 +10,6 @@ class ItemsController < ApplicationController
   def show
     @item =Item.find(params[:id])
     @user = User.find(@item.user_id)
-    @condition = Condition.find(@item.condition_id)
-    @shipping_charge = ShippingCharge.find(@item.shipping_charge_id)
-    @shipping_method = ShippingMethod.find(@item.shipping_method_id)
-    @delivery_area = DeliveryArea.find(@item.delivery_area_id)
-    @estimated_shipping_date = EstimatedShippingDate.find(@item.estimated_shipping_date_id)
     @grandchild = Category.find(6)
   end
 
