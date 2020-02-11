0d8a7b8588ee765d05cf55e468f554e5221a1769
honda0924 <honda0924@hotmail.co.jp>
2020-02-11 21:44:35 +0900

    add_images

--- app/assets/javascripts/item_image.js
+++ app/assets/javascripts/item_image.js
@@ -18,15 +18,76 @@ $(function(){
                 </div>`
     return html;
   }
+
+  $(document).on('change','input[type=file].upload-image',function(event){
+    var file=$(this).prop('files')[0];
+    var reader=new FileReader();
+    var img=$('<div class="img_view"><img></div>');
+    inputs.push($(this));
+    reader.onload=function(e){
+      var btn_wrapper = $('<div class="btn_wrapper"><div class="btn edit">編集</div><div class="btn delete">削除</div><div class="btn edit">');
+      img.append(btn_wrapper);
+      img.find('img').attr({
+        src: e.target.result
+      })
+    }
+    reader.readAsDataURL(file);
+    images.push(img);
+    if(images.length >= 5){
+      dropbox2.css({
+        'display': 'block'
+      })
+      dropbox.css({
+        'display': 'none'
+      })
+      $.each(images,function(index,image){
+        image.attr('data-image',index);
+        if (index < 5){
+          thumnail.append(image);
+        }else{
+          thumnail2.append(image);
+        }
+        dropbox2.css({
+          'width': `calc(620px - (124px * ${images.length - 5}))`
+        })
+      })
+      if(images.length == 9) {
+        dropbox2.find('p').replaceWith('<i class="fa fa-camera"></i>')
+      }
+    } else {
+        $('#thumnail').empty();
+        $.each(images, function(index, image) {
+          console.log(image);
+          image.attr('data-image', index);
+          thumnail.append(image);
+        })
+        dropbox.css({
+          'width': `calc(620px - (124px * ${images.length}))`
+        })
+      }
+      if(images.length == 4) {
+        dropbox.find('p').replaceWith('<i class="fa fa-camera"></i>')
+      }
+    if(images.length == 10) {
+      dropbox2.css({
+        'display': 'none'
+      })
+      return;
+    }
+    var new_image = $(`<input class="upload-image" name="item[images_attributes][${images.length}][image]" data-image= ${images.length} type="file" id="upload-image">`);
+    input_area.prepend(new_image);
+    console.log(images);
+  });
+
   if (window.location.href.match(/\/items\/\d+\/edit/)){
 
     $('.img_view').each(function(index){
       $(this).attr('data-image',index);
-      images.push(this);
+      images.push($(this));
     })
     $('input[id^=item_images_attributes]'+'input[id$=id]').each(function(index){
       $(this).attr('data-image',index);
-      inputs.push(this);
+      inputs.push($(this));
     });
     dropbox.css({
       'width': `calc(620px - (124px * ${images.length}))`
@@ -104,63 +165,7 @@ console.log($('input[name=\"item[images_attributes]['+target_image.data('image')
 
 
   }else{
-    $(document).on('change','input[type=file].upload-image',function(event){
-      var file=$(this).prop('files')[0];
-      var reader=new FileReader();
-      var img=$('<div class="img_view"><img></div>');
-      inputs.push($(this));
-      reader.onload=function(e){
-        var btn_wrapper = $('<div class="btn_wrapper"><div class="btn edit">編集</div><div class="btn delete">削除</div><div class="btn edit">');
-        img.append(btn_wrapper);
-        img.find('img').attr({
-          src: e.target.result
-        })
-      }
-      reader.readAsDataURL(file);
-      images.push(img);
-      if(images.length >= 5){
-        dropbox2.css({
-          'display': 'block'
-        })
-        dropbox.css({
-          'display': 'none'
-        })
-        $.each(images,function(index,image){
-          image.attr('data-image',index);
-          if (index < 5){
-            thumnail.append(image);
-          }else{
-            thumnail2.append(image);
-          }
-          dropbox2.css({
-            'width': `calc(620px - (124px * ${images.length - 5}))`
-          })
-        })
-        if(images.length == 9) {
-          dropbox2.find('p').replaceWith('<i class="fa fa-camera"></i>')
-        }
-      } else {
-          $('#thumnail').empty();
-          $.each(images, function(index, image) {
-            image.attr('data-image', index);
-            thumnail.append(image);
-          })
-          dropbox.css({
-            'width': `calc(620px - (124px * ${images.length}))`
-          })
-        }
-        if(images.length == 4) {
-          dropbox.find('p').replaceWith('<i class="fa fa-camera"></i>')
-        }
-      if(images.length == 10) {
-        dropbox2.css({
-          'display': 'none'
-        })
-        return;
-      }
-      var new_image = $(`<input class="upload-image" name="item[images_attributes][${images.length}][image]" data-image= ${images.length} type="file" id="upload-image">`);
-      input_area.prepend(new_image);
-    });
+    
     $(document).on('click','.delete',function(){
       var target_image=$(this).parent().parent();
       $.each(inputs,function(index,input){
@@ -187,6 +192,7 @@ console.log($('input[name=\"item[images_attributes]['+target_image.data('image')
         })
         $('input[type="file"].upload-image:first').after(input)
       })
+      $(".delete").prop("disabled",false);
       if (images.length >= 5) {
         dropbox2.css({
           'display': 'block'
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -89,6 +89,7 @@ class ItemsController < ApplicationController
 
   def update
     @item = Item.find(params[:id])
+    binding.pry
     if @item.update(item_params)
       redirect_to "/items/#{@item.id}"
     else
